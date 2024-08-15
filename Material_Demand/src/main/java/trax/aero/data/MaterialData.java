610089b5eb00de2bc91f68ed3babcb8c34888eac
UlisesFernandez <ulises.fernandez@trax.aero>
2024-08-14 22:03:27 -0400

    fixed transaction for check int 46

--- Material_Demand/src/main/java/trax/aero/data/MaterialData.java
+++ Material_Demand/src/main/java/trax/aero/data/MaterialData.java
@@ -826,7 +826,7 @@ public class MaterialData implements IMaterialData {
 			list = this.em.createQuery("SELECT p FROM PicklistDistribution p where ( p.externalCustRes IS NOT NULL and p.externalCustRes IS NOT NULL ) and "
 					+ " ( p.externalCustTo IS NULL)  AND p.id.transaction =:tra  "
 					+ "AND p.interfaceModifiedDate IS NOT NULL")
-					.setParameter("tra", "DISTRIBU")
+					.setParameter("tra", "REQUIRE")
 					.getResultList();
 					
 			if(list != null && list.size() > 0){		
