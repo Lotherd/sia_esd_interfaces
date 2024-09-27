877f2ee600c159bd3a9cc2cacf47ab156ec0617e
UlisesFernandez <ulises.fernandez@trax.aero>
2024-09-26 21:40:53 -0400

    bug fix

--- InstallRemoveSVO/src/main/java/trax/aero/data/InstallRemoveSvoData.java
+++ InstallRemoveSVO/src/main/java/trax/aero/data/InstallRemoveSvoData.java
@@ -342,7 +342,7 @@ public class InstallRemoveSvoData implements IInstallRemoveSvoData {
 					}
 					
 					if(rs1.getString(11) != null && !rs1.getString(11).isEmpty()) {
-						Inbound.setNotes(rs1.getString(11));
+						Inbound.setNotes(rs1.getString(11).substring(0, 7));
 					}
 					else {
 						Inbound.setNotes("");
